1275d9ccd8241f514b9abd86ebed8cdb30010981
Brodi Veaudry <brodiv1217@gmail.com>
2026-03-06 21:55:42 +0200

    Added formatted receipt output using JOptionPane

--- campusquickserve_ice/src/campusquickserve_ice/QuickServeApp.java
+++ campusquickserve_ice/src/campusquickserve_ice/QuickServeApp.java
@@ -22,6 +22,29 @@ public class QuickServeApp {
 
         // Create order object
         Order order = new Order(name, studentNumber, item, quantity, price);
+        
+        double subtotal = order.calculateSubtotal();
+double discount = order.calculateDiscount();
+double vat = order.calculateVAT();
+double total = order.calculateTotal();
+
+String receipt =
+        "===== Campus QuickServe Receipt =====\n\n" +
+        "Customer Name: " + name + "\n" +
+        "Student Number: " + studentNumber + "\n\n" +
+
+        "Item Ordered: " + item + "\n" +
+        "Quantity: " + quantity + "\n" +
+        "Price Per Item: R" + price + "\n\n" +
+
+        "Subtotal: R" + String.format("%.2f", subtotal) + "\n" +
+        "Discount Applied: R" + String.format("%.2f", discount) + "\n" +
+        "VAT (15%): R" + String.format("%.2f", vat) + "\n" +
+        "Final Total: R" + String.format("%.2f", total) + "\n"+
+        "Thank you for your order!" + "\n" +
+        "------------------------------------";
+
+JOptionPane.showMessageDialog(null, receipt);
 
     }
 
