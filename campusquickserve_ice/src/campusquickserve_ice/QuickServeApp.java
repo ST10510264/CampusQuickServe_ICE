47ce0f9edfa806e9349d94dbb6a930333d3cef82
Brodi Veaudry <brodiv1217@gmail.com>
2026-03-06 21:39:53 +0200

    Implemented GUI input using JOptionPane and created Order object

--- campusquickserve_ice/src/campusquickserve_ice/QuickServeApp.java
+++ campusquickserve_ice/src/campusquickserve_ice/QuickServeApp.java
@@ -1,12 +1,28 @@
-
 package campusquickserve_ice;
 
+import javax.swing.JOptionPane;
 
 public class QuickServeApp {
+
     public static void main(String[] args) {
 
-        // Main program will go here later
+        // Collect user input using GUI popups
+
+        String name = JOptionPane.showInputDialog("Enter Customer Name:");
+        String studentNumber = JOptionPane.showInputDialog("Enter Student Number:");
+        String item = JOptionPane.showInputDialog("Enter Item Ordered:");
+
+        int quantity = Integer.parseInt(
+                JOptionPane.showInputDialog("Enter Quantity:")
+        );
+
+        double price = Double.parseDouble(
+                JOptionPane.showInputDialog("Enter Price Per Item:")
+        );
+
+        // Create order object
+        Order order = new Order(name, studentNumber, item, quantity, price);
 
     }
-    
+
 }
