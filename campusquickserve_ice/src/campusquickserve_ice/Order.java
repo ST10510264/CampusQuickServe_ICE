ed3f6707688214a6fe4ea19b3ab44182b1f44a3e
Brodi Veaudry <brodiv1217@gmail.com>
2026-03-06 21:37:01 +0200

    Added Order class attributes, constructor and VAT constant

--- campusquickserve_ice/src/campusquickserve_ice/Order.java
+++ campusquickserve_ice/src/campusquickserve_ice/Order.java
@@ -2,6 +2,28 @@
 package campusquickserve_ice;
 
 
+/*
+ Order class stores all information related to a customer's order.
+ */
 public class Order {
-    
+
+    // Constant for VAT rate
+    public static final double VAT_RATE = 0.15;
+
+    // Order attributes
+    private String customerName;
+    private String studentNumber;
+    private String itemOrdered;
+    private int quantity;
+    private double pricePerItem;
+
+    // Constructor to initialise the order
+    public Order(String customerName, String studentNumber, String itemOrdered, int quantity, double pricePerItem) {
+        this.customerName = customerName;
+        this.studentNumber = studentNumber;
+        this.itemOrdered = itemOrdered;
+        this.quantity = quantity;
+        this.pricePerItem = pricePerItem;
+    }
+
 }
